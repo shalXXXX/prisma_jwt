86309d1782b2c2c29baee1431eb345df045c27ad
Shal0618 <hibiki.a.h.0618@gmail.com>
2023-07-03 17:24:29 +0900

    feat: login api route

---
+++ app/api/login/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { zLogin } from "../type";
+import { prisma } from "@/global/db";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parsedData = zLogin.parse(data);
+  const user = await prisma.user.findFirst({
+    where: {
+      email: parsedData.email,
+    }
+  });
+  if ( user === null) {
+    return new NextResponse("user not found", { status: 401 })
+  } else {
+    const comparePassword = async () => {
+      const isMatch = await compare(parsedData.password, user.password)
+      if(!isMatch) {
+        return new NextResponse("invalid password", { status: 401 })
+      }
+      const token = sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET!,
+        { expiresIn: "1h"}
+      )
+      
+      return new NextResponse(
+        `email: ${user.email}, name: ${user.name}`,
+        { status: 200 }
+      )
+    }
+    return await comparePassword();
+  }
+}
